f00f37d265ef33322d0408c64fcf4b1377316608
keny-stark <abdurakhmanov_i@iuca.kg>
2019-09-26 00:22:25 +0600

    сделал фикстуры и обновил формы

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from webapp.models import *
 
-# Register your models here.
+
+class TrackerAdmin(admin.ModelAdmin):
+    list_display = ['summary', 'description', 'type', 'status', 'created_at']
+
+
+admin.site.register(Tracker, TrackerAdmin)
+admin.site.register(Type)
+admin.site.register(Status)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Tracker(models.Model):
+    summary = models.TextField(max_length=100, null=False, blank=False, verbose_name='Summary')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Description')
+    status = models.ForeignKey('webapp.Status', related_name='tracker', on_delete=models.PROTECT, verbose_name='Status')
+    type = models.ForeignKey('webapp.Type', related_name='tracker', on_delete=models.PROTECT, verbose_name='Type')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Time of add')
+
+    def __str__(self):
+        return self.summary
+
+
+class Type(models.Model):
+    type = models.CharField(max_length=40, null=False, blank=False, verbose_name='Type')
+
+    def __str__(self):
+        return self.type
+
+
+class Status(models.Model):
+    status = models.CharField(max_length=40, null=False, blank=False, verbose_name='Status')
+
+    def __str__(self):
+        return self.status
