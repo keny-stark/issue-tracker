3d606b631f5341fb5efc8c18bd5ec3fb812c1ac3
keny-stark <abdurakhmanov_i@iuca.kg>
2019-10-14 00:44:55 +0600

    изменил вию трекира

--- source/webapp/views/tracker_view.py
+++ source/webapp/views/tracker_view.py
@@ -14,7 +14,24 @@ class IndexView(ListView):
     paginate_by = 3
     paginate_orphans = 1
 
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = Q(summary__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
 
     def get_search_form(self):
         return SimpleSearchForm(self.request.GET)
