af550a75936cb7318a024d6fe70c174e342c0f76
keny-stark <abdurakhmanov_i@iuca.kg>
2019-10-27 22:34:55 +0600

    добавил модель для регистрации юзера

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -7,12 +7,19 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        next_url = request.POST.get('next')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            if next_url:
+                return redirect(next_url)
             return redirect('webapp:index')
         else:
             context['has_error'] = True
+            context['next'] = next_url
+            context['username'] = username
+    else:
+        context = {'next': request.GET.get('next')}
     return render(request, 'login.html', context=context)
 
 
