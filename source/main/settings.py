081ecae568a1aea46d480e05c24425a7be843c06
keny-stark <abdurakhmanov_i@iuca.kg>
2019-12-08 15:51:24 +0600

    запустил новый стартап для работы с api

--- source/main/settings.py
+++ source/main/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
+    'rest_framework',
     'accounts',
-    'webapp'
+    'webapp',
+    'api_v2',
 
 ]
 
